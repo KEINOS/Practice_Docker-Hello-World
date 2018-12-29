9e6b941df7ca9704c312d03cdf775e5e1be4f236
KEINOS <github@keinos.com>
2018-12-29 12:19:02 +0900

    chore: Add comments

--- src/hello-world.py
+++ src/hello-world.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
 import argparse
 
-about  = 'This script prints "Hello World" or given string as an argument.'
+# Help msg of the app
+about  = 'This script prints "Hello World" or given string from the argument.'
+
+# Create arg parser
 parser = argparse.ArgumentParser(about)
 
+# Needs 2nd arg
 parser.add_argument(
     '-e', '--echo',
     help='Echoes the string followed.')
 
+# Only 1st as a flag
 parser.add_argument(
     '-s', '--say',
     help='Returns \'Hello World\'.',
     action='store_true')
 
+# Fetch arguments
 args = parser.parse_args()
 
+# Echo the 2nd arg or else just say hello
 if args.echo:
     print(args.echo)
 else:
